a07b04f6c69a649d2fc901141f207ca8c3a6e839
wangmeng <wangmeng255@gmail.com>
2016-05-21 23:11:13 -0700

    change if

--- js/app.js
+++ js/app.js
@@ -3,14 +3,10 @@ $(document).ready(function() {
 	for(var i=1; i<=n; i++)
 	{
 		var output = "";
-		if(!(i%3)) {
-				output += "fizz"; 
-				if(!(i%5)) output += "buzz";
-			}
-		else { 
-				if(!(i%5)) output += "buzz";
-				else output = i;
-			}
+		if(!(i%3)) output += "fizz"; 
+		if(!(i%5)) output += "buzz";
+		if(!output) output = i;
+		
 		$("script").first().before("<p>"+output+"</p>");
 	}
 });
