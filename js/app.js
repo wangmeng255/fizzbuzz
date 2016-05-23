32c3f12def1a7e14c6a8ae00cab1d33e69e9ba9d
wangmeng <wangmeng255@gmail.com>
2016-05-23 11:56:46 -0700

    input number

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-	var n = 100;
-	for(var i=1; i<=n; i++)
-	{
-		var output = "";
-		if(!(i%3)) output += "fizz"; 
-		if(!(i%5)) output += "buzz";
-		if(!output) output = i;
+	$("#fizzbuzz").click(function () {
+		var n = parseInt($("#num").val()); 
+		console.log(n);
+		if(!n) alert("please input a number");
+		fizzbuzz(n);
+	});
+	function fizzbuzz(n) {
+		for(var i=1; i<=n; i++)
+		{
+			var output = "";
+			if(!(i%3)) output += "fizz"; 
+			if(!(i%5)) output += "buzz";
+			if(!output) output = i;
 		
-		$("script").first().before("<p>"+output+"</p>");
+			$("script").first().before("<p>"+output+"</p>");
+		}
 	}
 });
