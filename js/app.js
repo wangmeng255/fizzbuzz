94b543947e6bebac3e239a59b84552200943f35e
wangmeng <wangmeng255@gmail.com>
2016-05-23 13:27:09 -0700

    input number

--- js/app.js
+++ js/app.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
 	$("#fizzbuzz").click(function () {
 		var n = parseInt($("#num").val()); 
 		if($("#num").val()!=String(n) || !n) alert("please input an integer");
-		fizzbuzz(n);
-	});
-	function fizzbuzz(n) {
-		for(var i=1; i<=n; i++)
-		{
-			var output = "";
-			if(!(i%3)) output += "fizz"; 
-			if(!(i%5)) output += "buzz";
-			if(!output) output = i;
-		
-			$("script").first().before("<p>"+output+"</p>");
+		else {
+			(function fizzbuzz(n) {
+				for(var i=1; i<=n; i++)
+				{
+					var output = "";
+					if(!(i%3)) output += "fizz"; 
+					if(!(i%5)) output += "buzz";
+					if(!output) output = i;
+				
+					$("script").first().before("<p>"+output+"</p>");
+				}
+			})(n);
 		}
-	}
+	});
 });
